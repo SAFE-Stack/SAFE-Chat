6081e2fb63d22ddfc4e259defa8308d775d07f06
olegz <olegzzzz@gmail.com>
2017-12-03 22:47:56 +0700

    attempt to detect socket close

--- src/Server/SocketFlow.fs
+++ src/Server/SocketFlow.fs
@@ -25,7 +25,7 @@ let private logger = Log.create "socketflow"
 // Provides websocket handshaking. Connects web socket to a pair of Source and Sync.
 // 'materialize'
 let handleWebsocketMessages (system: ActorSystem)
-    (materialize: IMaterializer -> Source<WsMessage, Akka.NotUsed> -> Sink<WsMessage, _> -> unit) (ws : WebSocket)
+    (materialize: IMaterializer -> Source<WsMessage, Akka.NotUsed> -> Sink<WsMessage, _> -> unit) (ws : WebSocket) ctx
     =
     let materializer = system.Materializer()
     let sourceActor, inputSource =
@@ -58,6 +58,7 @@ let handleWebsocketMessages (system: ActorSystem)
             | Ignore ->
                 ignored()
             | Close ->
+                logger.debug (Message.eventX "Close received. Sending Stop.")
                 stop()
         | _ ->
             ignored ()
@@ -70,28 +71,51 @@ let handleWebsocketMessages (system: ActorSystem)
 
     logger.debug (Message.eventX "materialized socket sink")
 
-    fun _ -> 
-        socket {
-            let loop = ref true
-            while !loop do
-                let! msg = ws.read()
-                
-                match msg with
-                | (Opcode.Text, data, true) -> 
-                    let str = Encoding.UTF8.GetString data
-                    sourceActor <! Text str
-                | (Ping, _, _) ->
-                    do! ws.send Pong emptyData true
-                | (Opcode.Close, _, _) ->
-                    // this finalizes the Source
-                    sourceActor <! Close
-                    do! ws.send Opcode.Close emptyData true
-                    loop := false
-                | _ -> ()
+    socket {
+        let mutable loop = true
+        while loop do
+            let! msg = ws.read()
+            
+            match msg with
+            | (Opcode.Text, data, true) -> 
+                let str = Encoding.UTF8.GetString data
+                sourceActor <! Text str
+            | (Ping, _, _) ->
+                do! ws.send Pong emptyData true
+            | (Opcode.Close, _, _) ->
+                logger.debug (Message.eventX "Received Opcode.Close")
+                // this finalizes the Source
+                sourceActor <! Close
+                do! ws.send Opcode.Close emptyData true
+                loop <- false
+            | _ -> ()
+    }
+
+let handleWebsocketMessagesWithError (system: ActorSystem)
+    (materialize: IMaterializer -> Source<WsMessage, Akka.NotUsed> -> Sink<WsMessage, _> -> unit) (ws : WebSocket) ctx = 
+   
+    let exampleDisposableResource = { new IDisposable with member __.Dispose() =
+        logger.debug (Message.eventX "Resource needed by websocket connection disposed")
         }
+   
+    async {
+        let! successOrError = handleWebsocketMessages system materialize ws ctx
+        logger.error (Message.eventX "Socket closed?")
+        
+        match successOrError with
+        // Success case
+        | Choice1Of2() -> ()
+        // Error case
+        | Choice2Of2(error) ->
+            // Example error handling logic here
+            logger.error (Message.eventX "Error: {error}" >> Message.setFieldValue "error" error)
+            exampleDisposableResource.Dispose()
+            
+        return successOrError
+    }
 
 /// Creates Suave socket handshaking handler
 let handleWebsocketMessagesFlow  (system: ActorSystem) (handler: Flow<WsMessage, WsMessage, Akka.NotUsed>) (ws : WebSocket) =
     let materialize materializer inputSource sink =
         inputSource |> Source.via handler |> Source.runWith materializer sink |> ignore
-    handleWebsocketMessages system materialize ws
+    handleWebsocketMessagesWithError system materialize ws
