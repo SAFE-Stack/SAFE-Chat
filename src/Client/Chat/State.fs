d18088aa5dad4430c611f576029b20fbab69e8cd
OlegZee <olegzzzz@gmail.com>
2019-05-31 02:21:10 +0700

    refactoring main app reducer

--- src/Client/Chat/State.fs
+++ src/Client/Chat/State.fs
@@ -15,39 +15,27 @@ let init () : ChatState * Cmd<Msg> =
     NotConnected, Cmd.tryOpenSocket socketAddr
 
 let update msg state : ChatState * Cmd<Msg> = 
-    match msg with
-    | ApplicationMsg amsg ->
-        match state with
-        | Connected chat ->
-            let newServerModel, cmd, serverMsg = RemoteServer.State.update amsg chat.serverData
-            let commands =
-                [ Cmd.map ApplicationMsg cmd ]
-                @ (serverMsg |> Option.map (Cmd.ofSocketMessage chat.socket) |> Option.toList)
 
-            Connected { chat with serverData = newServerModel }, Cmd.batch commands
-        | _ ->
-            console.error "Failed to process channel message. Server is not connected"
-            state, Cmd.none
-
-    | WebsocketMsg (socket, Opened) ->
+    match state, msg with
+    | NotConnected, WebsocketMsg (socket, Opened) ->
         Connecting socket, Cmd.ofSocketMessage socket Protocol.ServerMsg.Greets
 
-    | WebsocketMsg (_, Msg msg) ->
-        match state with
-        | Connecting socket ->
-            match msg with
-            | Protocol.Hello hello ->
-                let serverData, cmd = RemoteServer.State.init hello
-                Connected { socket = socket; serverData = serverData }, cmd |> Cmd.map ApplicationMsg
-            | unknown ->
-                console.error ("Unexpected message in Connecting state, ignoring ", unknown)
-                state, Cmd.none
+    | Connecting socket, WebsocketMsg (_, Msg (Protocol.Hello hello)) ->
+        let serverData, cmd = RemoteServer.State.init hello
+        Connected { socket = socket; serverData = serverData }, cmd |> Cmd.map ApplicationMsg
 
-        | Connected ({ serverData = serverData } as chat) ->
-            let newServerData, cmd = RemoteServer.State.chatUpdate msg serverData
-            Connected { chat with serverData = newServerData }, cmd |> Cmd.map ApplicationMsg
-        | other ->
-            console.info (sprintf "Socket message %A" other)
-            (other, Cmd.none)
+    | Connected chat, ApplicationMsg amsg ->
+        let newServerModel, cmd, serverMsg = RemoteServer.State.update amsg chat.serverData
+        let commands =
+            [ Cmd.map ApplicationMsg cmd ]
+            @ (serverMsg |> Option.map (Cmd.ofSocketMessage chat.socket) |> Option.toList)
 
-    | _ -> state, Cmd.none
+        Connected { chat with serverData = newServerModel }, Cmd.batch commands
+
+    | Connected ({ serverData = serverData } as chat), WebsocketMsg (_, Msg msg) ->
+        let newServerData, cmd = RemoteServer.State.chatUpdate msg serverData
+        Connected { chat with serverData = newServerData }, cmd |> Cmd.map ApplicationMsg
+
+    | _, msg ->
+        console.error ("Failed to process message", msg)
+        state, Cmd.none
